d604a50daa327ec6e8f99c0027c033f56658c029
capbmaster <c.portillo@uniandes.edu.co>
2025-04-25 07:16:59 -0500

    Feat: Initial configs

---
+++ src/app/plant/components/list-plants/list-plants.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListPlantsComponent } from './list-plants.component';
+
+describe('ListPlantsComponent', () => {
+  let component: ListPlantsComponent;
+  let fixture: ComponentFixture<ListPlantsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ListPlantsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListPlantsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/plant/components/list-plants/list-plants.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-plants',
+  standalone: false,
+  templateUrl: './list-plants.component.html',
+  styleUrl: './list-plants.component.css'
+})
+export class ListPlantsComponent {
+
+}
